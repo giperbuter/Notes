63fa135ba2c4611dbb52547a3a1a4c1ad04704f6
Yani842 <85073979+Yani842@users.noreply.github.com>
2022-03-05 22:36:22 +0200

    search bar animation

--- elements/Bar.tsx
+++ elements/Bar.tsx
@@ -1,25 +1,33 @@
 import React, { useRef, useState } from 'react'
-import { StyleSheet, View, KeyboardAvoidingView, TextInput, Animated, Keyboard } from 'react-native';
+import { StyleSheet, View, KeyboardAvoidingView, TextInput, Animated, Keyboard, Pressable } from 'react-native';
 
 import SearchIcon from './search-icon.jsx'
 import PlusIcon from './plus-icon.jsx'
 import FinishIcon from './finish-icon.jsx'
 
+// one animation variable (1-0) with interpolation
+
 let Bar = (props) => {
   const [isSearchIcon, setIsSearchIcon] = useState(true);
   const [isFinishIcon, setIsFinishIcon] = useState(false);
+  const [enableKeyboard, setEnableKeyboard] = useState(true);
+  const [isSearchBar, setIsSearchBar] = useState(false);
+  // const [isChanging, setIsChanging] = useState(false);
 
-  const SearchOpacity = useRef(new Animated.Value(1)).current;
-  const FinishOpacity = useRef(new Animated.Value(0)).current;
+  const searchOpacity = useRef(new Animated.Value(1)).current;
+  const finishOpacity = useRef(new Animated.Value(0)).current;
+  const homeOpacity = useRef(new Animated.Value(0)).current;
+  const createBarOpacity = useRef(new Animated.Value(1)).current;
+  const changeAnim = useRef(new Animated.Value(1)).current;
 
   let keyboardShowing = () => {
-    Animated.timing(SearchOpacity, {toValue: 0, duration: 150, useNativeDriver: true}).start();
-    Animated.timing(FinishOpacity, {toValue: 1, duration: 150, useNativeDriver: true}).start();
+    Animated.timing(searchOpacity, { toValue: 0, duration: 150, useNativeDriver: true }).start();
+    Animated.timing(finishOpacity, { toValue: 1, duration: 150, useNativeDriver: true }).start();
     setIsFinishIcon(true);
   }
   let keyboardHiding = () => {
-    Animated.timing(SearchOpacity, {toValue: 1, duration: 150, useNativeDriver: true}).start();
-    Animated.timing(FinishOpacity, {toValue: 0, duration: 150, useNativeDriver: true}).start();
+    Animated.timing(searchOpacity, { toValue: 1, duration: 150, useNativeDriver: true }).start();
+    Animated.timing(finishOpacity, { toValue: 0, duration: 150, useNativeDriver: true }).start();
     setIsSearchIcon(true);
   }
   let keyboardShowed = () => {
@@ -33,20 +41,81 @@ let Bar = (props) => {
   Keyboard.addListener('keyboardDidShow', keyboardShowed);
   Keyboard.addListener('keyboardDidHide', keyboardHid);
 
+  let searchPress = () => {
+    setIsSearchBar(false);
+    if (isSearchBar) {
+      Animated.timing(changeAnim, { toValue: 1, duration: 2000, useNativeDriver: false }).start(() => { Animated.timing(homeOpacity, { toValue: 0, duration: 200, useNativeDriver: true }).start() })
+      Animated.timing(createBarOpacity, { toValue: 1, duration: 2000, useNativeDriver: false }).start();
+      setIsSearchBar(false);
+      setEnableKeyboard(true);
+    } else if (isSearchBar == false) {
+      Animated.timing(changeAnim, { toValue: 0, duration: 200, useNativeDriver: false }).start(() => { Animated.timing(homeOpacity, { toValue: 1, duration: 200, useNativeDriver: true }).start(); setIsSearchBar(true); });
+      Animated.timing(createBarOpacity, { toValue: 0, duration: 200, useNativeDriver: false }).start();
+      setEnableKeyboard(false);
+    }
+  }
+
   return (
     <KeyboardAvoidingView behavior='padding' keyboardVerticalOffset={20} style={styles.keyboardView}>
-      <View style={styles.left}>
-        <TextInput
-          placeholder='Tap to create a Note!'
-          style={styles.leftTextinput}
-        />
-      </View>
-      <View style={styles.right}>
-        <View style={styles.iconContainer}>
-          {isSearchIcon ? <Animated.View style={[styles.icon, {opacity: SearchOpacity}]}><SearchIcon /></Animated.View> : <></>}
-          {isFinishIcon ? <Animated.View style={[styles.icon, {opacity: FinishOpacity}]}><FinishIcon /></Animated.View> : <></>}
-        </View>
-      </View>
+      <Animated.View style={[styles.left, { flexBasis: changeAnim.interpolate({ inputRange: [0, 1], outputRange: ['14%', '85%'] }) }]}>
+        <Animated.View style={[styles.leftTextinputContainer, { width: changeAnim.interpolate({ inputRange: [0, 1], outputRange: [40, 305] }) }]}>
+          {(!isSearchBar) ?
+            <Animated.View style={{ opacity: createBarOpacity }}>
+              <TextInput placeholder='Tap to create a Note!' style={styles.leftTextinput} editable={enableKeyboard} />
+            </Animated.View>
+            :
+            <View style={[styles.leftIcon]}>
+              <Animated.View style={{ opacity: homeOpacity }}>
+                <PlusIcon />
+              </Animated.View>
+            </View>
+          }
+        </Animated.View>
+      </Animated.View>
+      <Animated.View style={[styles.right, { flexBasis: changeAnim.interpolate({ inputRange: [0, 1], outputRange: ['85%', '14%'] }) }]}>
+        <Animated.View style=
+          {
+            [
+              styles.iconContainer,
+              {
+                borderRadius: changeAnim.interpolate(
+                  {
+                    inputRange: [0, 1],
+                    outputRange: [10, 50]
+                  }
+                ),
+                width: changeAnim.interpolate(
+                  {
+                    inputRange: [0, 1],
+                    outputRange: [305, 40]
+                  }
+                )
+              }
+            ]
+          }>
+          {isSearchIcon ?
+            // <Pressable onPress={searchPress}>
+            <Animated.View style={[styles.icon, { opacity: searchOpacity }]}>
+              <Pressable onPress={searchPress} style={{}}>
+                <SearchIcon />
+              </Pressable>
+            </Animated.View>
+            : <></>}
+          {isFinishIcon ?
+            <Animated.View style={[styles.icon, { opacity: finishOpacity }]}>
+              <FinishIcon />
+            </Animated.View>
+            : <></>}
+          {isSearchBar ?
+            <Animated.View style={[styles.rightTextinputContainer, { opacity: homeOpacity }]}>
+              <TextInput
+                placeholder='Tap to Search for a Note!'
+                style={styles.rightTextinput}
+              />
+            </Animated.View>
+            : <></>}
+        </Animated.View>
+      </Animated.View>
     </KeyboardAvoidingView>
   )
 }
@@ -55,30 +124,60 @@ module.exports = Bar;
 
 const styles = StyleSheet.create({
   right: {
-    flexBasis: '15%',
     flex: 1,
     alignItems: 'flex-end',
   },
   left: {
-    flexBasis: '85%',
     flex: 1,
+    alignItems: 'flex-start',
   },
   iconContainer: {
     position: 'absolute',
     width: 40,
     height: 40,
-    borderRadius: 50,
     backgroundColor: 'rgba(255, 255, 255, .7)',
   },
-  icon : {
+  icon: {
     position: 'absolute',
     width: 40,
     height: 40,
     padding: 3,
+    right: 0,
+  },
+  leftIcon: {
+    position: 'absolute',
+    width: 40,
+    height: 40,
+    backgroundColor: '#ddd',
+    padding: 3,
+    right: 0,
+    borderRadius: 50,
+  },
+  rightTextinputContainer: {
+    height: 40,
+    width: 250,
+    left: 0,
+  },
+  rightTextinput: {
+    height: 40,
+    width: 250,
+    left: 0,
+    paddingLeft: 15,
+    paddingRight: 15,
+    fontSize: 17,
+    backgroundColor: "#0000",
+    borderRadius: 10,
+  },
+  leftTextinputContainer: {
+    height: 40,
+    width: '100%',
+    backgroundColor: "#ddd",
+    borderRadius: 25,
   },
   leftTextinput: {
     height: 40,
     width: '100%',
+    left: 0,
     paddingLeft: 15,
     paddingRight: 15,
     fontSize: 17,
